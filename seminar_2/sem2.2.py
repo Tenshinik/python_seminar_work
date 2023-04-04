44c1fad39a5c29966d44d5d175be01e9563ff40b
Tenshinik <@tenshinik@gmail.com>
2023-04-04 19:58:52 +0800

    new project

--- seminar_2/sem2.2.py
+++ seminar_2/sem2.2.py
@@ -1,12 +1,18 @@
 # Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.
 
 
+#n = int(input())
+#m = 1
+#while m < n:
+ #   if(m < n):
+  #      if (m * 2 > n):
+       #     break
+ #   else:
+     #   m = m * 2
+       # print(m)
+
 n = int(input())
-m = 1
-while m < n:
-    if(m < n):
-        if (m * 2 > n):
-            break
-    else:
-        m = m * 2
-        print(m)
+i = 0
+while 2 ** i <= n:
+    print(2 ** i)
+    i += 1
